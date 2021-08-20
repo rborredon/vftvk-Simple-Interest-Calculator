89f5d5afa9c2c943b9bf5c922130e4a2fa61a7a1
rborredon <48650353+rborredon@users.noreply.github.com>
2021-08-20 12:49:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+
+    if (principal <= 0)
+    {
+        document.getElementById("result").innerHTML="";
+        alert("Enter a positive number");
+    }
+    else
+    {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";    
+    }
 }
 
 function updateRate() 
