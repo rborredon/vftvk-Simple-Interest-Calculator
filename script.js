4e805f0834f04d3feb733647d2039730f5e36ff7
rborredon <48650353+rborredon@users.noreply.github.com>
2021-08-20 12:51:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+//Compute the awaited value
 function compute()
 {
     var principal = document.getElementById("principal").value;
 
+    //Checking if principale is positive or not
     if (principal <= 0)
     {
+        //Negative or null value
         document.getElementById("result").innerHTML="";
         alert("Enter a positive number");
     }
     else
     {
+        //Positive value
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
@@ -18,6 +22,7 @@ function compute()
     }
 }
 
+//Update the rate display
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value+"%";
